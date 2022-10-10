47b70133897c1e14715611bac0e5f8b62f1cc405
Zizhan <zizhanzhou24@gmail.com>
2022-10-10 11:41:07 -0400

    updated README and database setup

--- flaskblog.py
+++ flaskblog.py
@@ -1,9 +1,40 @@
-from turtle import title
+from datetime import datetime
+from email.policy import default
 from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
-app = Flask(__name__)
 
+app = Flask(__name__)
 app.config['SECRET_KEY'] = 'e03e741a9d52151f0d7515df04b0565c'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username},{self.email}, {self.image_file}')"
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.title},{self.date_posted}')"
+
+# with app.app_context():
+#         db.create_all()
+#         user_1 = User(username='zizhan', email='z@demo.com', password='password')
+#         db.session.add(user_1)
+# user_2 = User(username='xixi', email='xixi@demo.com', password='password')
 
 posts = [
     {
