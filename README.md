0d9ed92c34c9cee390469022b540405fbc2ae66a
Zizhan <zizhanzhou24@gmail.com>
2022-10-18 14:18:20 -0400

    refactored code using blueprints and configuration

--- README.md
+++ README.md
@@ -1,10 +1,16 @@
 <h1>Flask Blog App<h1>
 <p>
-In order to create a virtual environment, type in the following command:
+<section>In order to create a virtual environment, type in the following command:
 py -3 -m venv .venv
+Command for activating virtual env:
 .venv\scripts\activate 
+</section>
+<section>
+To run the app:
 flask --app flaskblog.py run
 flask --app flaskblog.py --debug run
 $env:FLASK_APP = "run"
 python -m flask run
+</section>
 </p>
+
--- flaskblog/users/forms.py
+++ flaskblog/users/forms.py
@@ -1,10 +1,9 @@
-from tokenize import String
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
-from flaskblog.models import User
 from flask_login import current_user
+from flaskblog.models import User
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=2,max=20)])
@@ -48,10 +47,6 @@ class UpdateAccountForm(FlaskForm):
             if email:
                 raise ValidationError('The email is taken, please choose a different one')
 
-class PostForm(FlaskForm):
-    title = StringField('Title', validators=[DataRequired()])
-    content = TextAreaField('Content', validators=[DataRequired()])
-    submit = SubmitField('Post')
 
 class RequestResetForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
@@ -67,10 +62,3 @@ class ResetPasswordForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Reset Password')
-
-
-
-
-
-
-
